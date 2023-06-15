fdd2aaabb68c16d202caefb9907a093349cebaca
Jonatas Cruz <131266716+JntCruz99@users.noreply.github.com>
2023-06-14 21:04:00 -0300

    first commit

---
+++ src/main/java/com/example/demo/controller/FoodController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import com.example.demo.food.Food;
+import com.example.demo.food.FoodRequestDTO;
+import com.example.demo.food.FoodResponseDTO;
+import com.example.demo.food.FoodRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("food")
+public class FoodController {
+
+    @Autowired
+    private FoodRepository repository;
+
+    @PostMapping
+    public void saveFood(@RequestBody FoodRequestDTO data){
+        Food foodData = new Food(data);
+        repository.save(foodData);
+        return;
+    }
+
+    @GetMapping
+    public List<FoodResponseDTO> getAll(){
+
+        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
+        return foodList;
+    }
+}
