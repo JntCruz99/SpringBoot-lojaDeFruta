f1687b7b8076bbef6a8b4e268820212218474b98
Jonatas Cruz <131266716+JntCruz99@users.noreply.github.com>
2023-10-04 18:26:11 -0300

    criando deploy sem banco de dados

--- src/main/java/com/example/demo/controller/FoodController.java
+++ src/main/java/com/example/demo/controller/FoodController.java
@@ -1,7 +1,6 @@
 package com.example.demo.controller;
 
 import com.example.demo.food.Food;
-import com.example.demo.food.FoodRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,18 +10,11 @@ import java.util.List;
 @RequestMapping("food")
 public class FoodController {
 
-    @Autowired
-    private FoodRepository repository;
-
-    @PostMapping
-    public void saveFood(@RequestBody Food data){
-        repository.save(data);
-    }
 
     @GetMapping
-    public List<Food> getAll(){
+    public String hello(){
 
-        List<Food> foodList = repository.findAll();
-        return foodList;
+
+        return "helloWord";
     }
 }
