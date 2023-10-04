a053ed25cd5b184dad2178057fe21a2ee0b2119c
Jonatas Cruz <131266716+JntCruz99@users.noreply.github.com>
2023-10-04 19:14:26 -0300

    agr vai

--- src/main/java/com/example/demo/food/Food.java
+++ src/main/java/com/example/demo/food/Food.java
@@ -2,14 +2,12 @@ package com.example.demo.food;
 
 import lombok.*;
 
-import javax.persistence.*;
 
-@Table(name = "foods")
-@Entity(name = "foods ")
+
+
 @Data
 public class Food {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
     private String title;
     private String image;
